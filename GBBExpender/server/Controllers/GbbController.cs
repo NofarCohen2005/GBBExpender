688d3461e38e485fa87609e49fcef0f351af611b
NofarCohen2005 <bupr2005@gmail.com>
2026-03-25 17:53:09 +0200

    fix

--- GBBExpender/server/Controllers/GbbController.cs
+++ GBBExpender/server/Controllers/GbbController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 
-namespace GbbGenerator.Controllers
+namespace GbbExpender.Controllers
 {
     public class PropertyModel
     {
